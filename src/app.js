a4a8a77ca4246c3f83ecc6ec30d0db359e219807
CuneytGuley <cuneyt.guley@outlook.com>
2021-10-20 22:49:44 +0300

    18 Ekim

---
+++ src/app.js
@@ -0,0 +1,34 @@
+console.log("Merhaba Kodlama.io")
+
+//JS type safe değildir.
+let dolarBugun = 9.30
+let dolarDun = 9.20
+
+
+{
+    let dolarDun = 9.10
+}
+
+console.log(dolarDun)
+
+const euroDun = 11.2 
+// euroDun = 11 ** const ile atanan veriyi değiştiremezsin.
+
+console.log(euroDun)
+
+// array
+// camelCasing
+// array içerisinde herşey olabilir, string, number, array
+// örnek : https://jsonplaceholder.typicode.com/users 
+
+let konutKredileri = ["Konut kredisi", "Emlak konut kredisi", "Kamu konut kredisi", "Özel konut kredisi"]
+
+console.log("<ul>")
+for(let i = 0; i<konutKredileri.length;i++){
+    console.log("<li>" + konutKredileri[i] + "</li>")
+}
+console.log("</ul>")
+
+
+console.log(konutKredileri)
+
